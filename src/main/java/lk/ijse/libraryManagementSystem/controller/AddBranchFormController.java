891dd00a8fd1822185c9f6acf93e80941c5b65f5
User <thiwandika.whs02@gmail.com>
2024-03-14 20:02:24 +0530

    Validate Branch

--- src/main/java/lk/ijse/libraryManagementSystem/controller/AddBranchFormController.java
+++ src/main/java/lk/ijse/libraryManagementSystem/controller/AddBranchFormController.java
@@ -22,6 +22,7 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class AddBranchFormController {
 
@@ -84,40 +85,87 @@ public class AddBranchFormController {
     }
     @FXML
     void addBtnOnAction(ActionEvent event) throws IOException {
-        boolean checked = checkDuplicates();
-        if (checked){
+        boolean isValid=validateBranch();
+        if (isValid) {
+            boolean checked = checkDuplicates();
+            if (checked) {
 
-            String addressText = txtAddress.getText();
-            String contactText = txtContact.getText();
-            String emailText = txtEmail.getText();
-            String nameText = txtName.getText();
-            String idText = labelId.getText();
-            boolean saved = branchBo.updateBranch(new BranchDto(idText, nameText, addressText, contactText, emailText,branchDto.getUsers(),branchDto.getBooks()));
-            if (saved){
-                new Alert(Alert.AlertType.CONFIRMATION,"Successfully Updated").show();
-            }
-            else {
-                new Alert(Alert.AlertType.ERROR,"Can not Update").show();
+                String addressText = txtAddress.getText();
+                String contactText = txtContact.getText();
+                String emailText = txtEmail.getText();
+                String nameText = txtName.getText();
+                String idText = labelId.getText();
+                boolean saved = branchBo.updateBranch(new BranchDto(idText, nameText, addressText, contactText, emailText, branchDto.getUsers(), branchDto.getBooks()));
+                if (saved) {
+                    new Alert(Alert.AlertType.CONFIRMATION, "Successfully Updated").show();
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Can not Update").show();
+                }
+            } else {
+                String addressText = txtAddress.getText();
+                String contactText = txtContact.getText();
+                String emailText = txtEmail.getText();
+                String nameText = txtName.getText();
+                String idText = labelId.getText();
+                boolean saved = branchBo.saveBranch(new BranchDto(idText, nameText, addressText, contactText, emailText, new ArrayList<>(), new ArrayList<>()));
+                if (saved) {
+                    new Alert(Alert.AlertType.CONFIRMATION, "Successfully Saved").show();
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Can not Save").show();
+                }
             }
         }
-        else {
-            String addressText = txtAddress.getText();
-            String contactText = txtContact.getText();
-            String emailText = txtEmail.getText();
-            String nameText = txtName.getText();
-            String idText = labelId.getText();
-            boolean saved = branchBo.saveBranch(new BranchDto(idText, nameText, addressText, contactText, emailText,new ArrayList<>(),new ArrayList<>()));
-            if (saved){
-                new Alert(Alert.AlertType.CONFIRMATION,"Successfully Saved").show();
-            }
-            else {
-                new Alert(Alert.AlertType.ERROR,"Can not Save").show();
-            }
-        }
-
 
 
     }
+
+    private boolean validateBranch() {
+        String name=txtName.getText();
+        if(name.isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"Branch name field empty").show();
+            return false;
+        }
+        if(txtAddress.getText().isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"Branch Address field empty").show();
+            return false;
+        }
+        boolean matchAddress = Pattern.matches("^[0-9A-Za-z\\s.,#-]+$",txtAddress.getText());
+        if(!matchAddress){
+            new Alert(Alert.AlertType.ERROR,"Invalid address").show();
+            return  false;
+        }
+        String email = txtEmail.getText();
+        if(email.isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"Email address field empty").show();
+            return false;
+
+        }
+        boolean matchEmail= Pattern.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"" +
+                "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")" +
+                "@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])" +
+                "|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:" +
+                "[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",email);
+        if(!matchEmail){
+            new Alert(Alert.AlertType.ERROR,"Invalid email").show();
+            return  false;
+        }
+
+        String contact = txtContact.getText();
+        if(contact.isEmpty()){
+            new Alert(Alert.AlertType.ERROR,"contact field empty").show();
+            return false;
+
+        }
+        boolean matchContact= Pattern.matches("^0[1-9]\\d{8}$",contact);
+        if(!matchContact){
+            new Alert(Alert.AlertType.ERROR,"Invalid contact number").show();
+            return  false;
+        }
+
+
+        return true;
+    }
+
 
     @FXML
     void cancelBtnOnAction(ActionEvent event) {
